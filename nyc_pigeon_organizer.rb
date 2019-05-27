160919c2729e7bcb3267b13969e9039092a855b5
Ryan Bravo <rrbravo2@gmail.com>
2019-05-27 06:59:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,2 +1,7 @@
 def nyc_pigeon_organizer(data)
-
+  names = []
+  pigeon_hash = {}
+ data.each do |attribute, items|
+    items.each do |feature, arr|
+      arr.each do |name|
+        names << name if !names.include?(name)
