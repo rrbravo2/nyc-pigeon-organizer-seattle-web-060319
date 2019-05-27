3bcc5e949c72f3e91459afe810ce9ac538c4c350
Ryan Bravo <rrbravo2@gmail.com>
2019-05-27 06:54:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,9 +19,10 @@ new_hash.each do |name, values|
       array.each do [element]
       if element == name && new_hash_value == attribute
        new_hash[name][new_hash_value] << value.to_s 
-        end
-      end
-    end
-  end
+         end
+       end
+     end
+   end
+ end
 end
 new_hash
