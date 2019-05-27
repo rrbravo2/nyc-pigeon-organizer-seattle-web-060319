88041b42e1e8c1bd835301f0f8de8262707027b5
Ryan Bravo <rrbravo2@gmail.com>
2019-05-27 06:59:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,28 +1,2 @@
 def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |attribute, values|
-    values.each do |value, array|
-      array.each do |name|
-        if new_hash[name] == NIL
-          new_hash[name] = {}
-          new_hash[name][attribute] = []
-        else 
-          new_hash[name][attribute] = []
-     end
-    end
-  end
-end
-new_hash.each do |name, values|
-  values.each do |new_hash_value, array|
-    data.each do |attribute, values|
-      values.each do |value, array|
-      array.each do [element]
-      if element == name && new_hash_value == attribute
-       new_hash[name][new_hash_value] << value.to_s 
-        end
-      end
-    end
-   end
-  end
- end
-end
+
